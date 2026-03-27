9e06ac7de8cadfbff84f850590314c194a5e30a2
Null_ <kauannycolassad@gmail.com>
2026-03-27 14:18:38 -0300

    Adicionando meus projetos em C

---
+++ Area do quadrado.c
@@ -0,0 +1,15 @@
+//Area do quadrado:
+#include <stdio.h>
+
+int main() {
+    float lado, area;
+    
+    printf("Digite o lado do quadrado: ");
+    scanf("%f", &lado);
+    
+    area = lado * lado;
+    
+    printf("A área do quadrado é: %.f",area);
+    
+    return 0;
+}
